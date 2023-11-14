91fbdcd27b7921dc120fc02a08f7c05277aeb9be
LindaKat <lindakt16@gmail.com>
2023-11-14 02:02:14 -0600

    finally got data pulling from content collection

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { z, defineCollection } from "astro:content";
+
+const gamesCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    photo: z.string(),
+    photoAlt: z.string(),
+    tagline: z.string(),
+    tags: z.array(z.string()),
+    title: z.string(),
+    link: z.string(),
+  }),
+});
+
+export const collections = {
+  games: gamesCollection,
+};
